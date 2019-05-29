7b384d12d32522f59547813a44370ae958e39bef
Arnulfo Rodriguez <arodriguez@backcountry.com>
2019-05-29 10:15:48 -0600

    simplified solution

--- src/main/scala/exercises/Solution.scala
+++ src/main/scala/exercises/Solution.scala
@@ -29,10 +29,6 @@ object Solution {
 
   case class Sub(x: Expression, y: Expression) extends Expression
 
-  case class Pow(base: Expression, exponent: Expression) extends Expression
-
-  case class Modulus(dividend: Expression, divisor: Expression) extends Expression
-
   case class UnaryPlus(innerExpression: Expression) extends Expression
 
   case class UnaryMinus(innerExpression: Expression) extends Expression
@@ -148,8 +144,8 @@ object Solution {
 
 
   object Evaluator {
-    val p = Literal(1000000007)
-    val p_minus_two = Literal(1000000005)
+    val p = 1000000007l
+    val p_minus_two = 1000000005l
   }
 
   class Evaluator() {
@@ -157,71 +153,34 @@ object Solution {
     import Evaluator._
 
     def evaluate(tree: Expression): Long = {
-      val transformed = transform(Modulus(tree, p))
-      println(transformed)
-      val result = evaluateRec(transformed)
-      result
+      val result = evaluateRec(tree)
+      if (result < 0) p + result else result
     }
 
     private def evaluateRec(expression: Expression): Long = {
       val result = expression match {
         case Literal(x) => x
-        case Add(x, y) => evaluateRec(x) + evaluateRec(y)
-        case Mult(x, y) => evaluateRec(x) * evaluateRec(y)
-        case Sub(x, y) => evaluateRec(x) - evaluateRec(y)
-        case UnaryMinus(x) => -1 * evaluateRec(x)
-        case Pow(x, y) =>
-          scala.math.pow(evaluateRec(x), evaluateRec(y)).toLong
-        case Modulus(Pow(x, y), m) =>
-          val eval_x = evaluateRec(x)
-          val eval_y = evaluateRec(y)
-          val eval_m = evaluateRec(m)
-          fastExponentation(eval_x,eval_y,eval_m)
-        case Modulus(x, y) =>
-          evaluateRec(x) % evaluateRec(y)
+        case Add(x, y) => (evaluateRec(x) + evaluateRec(y)) % p
+        case Mult(x, y) => (evaluateRec(x) * evaluateRec(y)) % p
+        case Sub(x, y) => (evaluateRec(x) - evaluateRec(y)) % p
+        case UnaryMinus(x) => (-1 * evaluateRec(x)) % p
+        case Div(x,y) => (evaluateRec(x) * fastExponentiation(evaluateRec(y),p_minus_two,p)) % p
       }
       //println(s"Expression $expression evaluated to $result")
       result
     }
 
-    @tailrec
-    private def modularExponentiation(x: Long, y: Long, m: Long, acc: Long): Long = y match {
-      case 0 => acc
-      case _ => modularExponentiation(x, y - 1, m, acc * x % m)
-    }
 
-    private def fastExponentation(a: Long, b: Long, m: Long): Long = b match {
+    private def fastExponentiation(a: Long, b: Long, m: Long): Long = b match {
       case 0 => 1
-      case even if even % 2 == 0 => scala.math.pow(fastExponentation(a, b / 2, m), 2).toLong % m
-      case _ => (scala.math.pow(fastExponentation(a, (b - 1) / 2, m), 2).toLong * a) % m
+      case even if even % 2 == 0 =>
+        val result = fastExponentiation(a, b / 2, m) % m
+        (result * result) % m
+      case _ =>
+        val result = fastExponentiation(a, (b - 1) / 2, m) % m
+        (((result * result) % m) * a) % m
     }
 
-
-    def isAlreadySimplified(x: Expression, y: Expression): Boolean = (x, y) match {
-      case (Modulus(_, _), Modulus(_, _)) => true;
-      case _ => false
-    }
-
-    private def transform(expression: Expression): Expression = expression match {
-      case Modulus(Modulus(a, b), c) if b == c => transform(Modulus(a, b))
-      case Modulus(Mult(x,y),m) => transform(Mult(Modulus(x,m),Modulus(y,m)))
-      case Div(x, y) => transform(Modulus(Mult(x, Pow(y, p_minus_two)), p))
-      case Pow(Pow(a, b), c) => transform(Pow(a, Mult(b, c)))
-      case Add(x, y) => Add(transform(x), transform(y))
-      case Mult(x, y) => Mult(transform(x), transform(y))
-      case Sub(x, y) => Sub(transform(x), transform(y))
-      case Modulus(x, y) =>
-        val transformed_x = transform(x)
-        val transformed_y = transform(y)
-        val result = Modulus(transformed_x, transformed_y)
-        if (transformed_x == x && transformed_y == y) {
-          result
-        } else {
-          transform(result)
-        }
-      case x => x
-
-    }
 
   }
 
