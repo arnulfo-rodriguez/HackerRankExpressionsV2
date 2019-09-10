ca4a9a2c390c131445c3400309d8762f9f39de84
hudson <hudson@jesusfreak2>
2019-09-10 07:53:15 -0600

    getting closer

--- src/main/scala/exercises/Solution.scala
+++ src/main/scala/exercises/Solution.scala
@@ -8,40 +8,43 @@ import scala.collection.mutable
 object Solution {
 
 
-
-  def addSorted(i: Int,priorityQueue: Seq[Int]) = {
+  def addSorted(i: Int, priorityQueue: Seq[Int]) = {
     @tailrec
     def addSortedRec(newElement: Int, existing: Seq[Int], acc: Seq[Int]): Seq[Int] = existing match {
       case first :: rest if newElement <= first => acc ++ (newElement :: first :: rest)
       case first :: rest => addSortedRec(newElement, rest, acc :+ first)
       case _ => acc :+ newElement
     }
-    addSortedRec(i,priorityQueue,Seq())
+
+    addSortedRec(i, priorityQueue, Seq())
   }
 
   // Complete the solve function below.
   def solve(array: Array[Int]): Long = {
 
-    def countWhile(priorityQueue: Seq[Int],  f:  Int => Boolean) : Long = {
-      def spanRec(iterator: Seq[Int], acc: Long): Long = iterator match {
-        case first::rest if f(first) => spanRec(rest,acc + 1)
+    def countWhile(priorityQueue: Seq[Int], max: Int, f: (Int, Int) => Boolean): Long = {
+      def spanRec(i: Int, iterator: Seq[Int], acc: Long): Long = iterator match {
+        case first :: rest if i == 0 && f(first,max) => spanRec(i+1,rest, acc + 1)
+        case first :: rest if i == 0 && f(first,max) => spanRec(i+1,rest, acc + 1)
         case _ => acc
       }
-      spanRec(priorityQueue, 0l)
+
+      spanRec(0,priorityQueue, 0l)
     }
 
-    def solveRec(current: Int, priorityQueue: Seq[Int], max : Int, total : Long): Long = current match {
+    def solveRec(current: Int, priorityQueue: Seq[Int], max: Int, total: Long): Long = current match {
       case 0 =>
-        solveRec(1,addSorted(array(current),priorityQueue),array(current),total)
+        solveRec(1, addSorted(array(current), priorityQueue), array(current), total)
       case i if i < array.length => {
         val iThElement = array(i)
         val newMax = math.max(max, iThElement)
-        val matchesCount = countWhile(priorityQueue,v => (v.toLong * iThElement.toLong) <= newMax)
-        solveRec(i + 1,addSorted(iThElement,priorityQueue),newMax, total + matchesCount)
+        val matchesCount = countWhile(priorityQueue, newMax, (v, m) => (v.toLong * iThElement.toLong) <= m)
+        solveRec(i + 1, addSorted(iThElement, priorityQueue), newMax, total + matchesCount)
       }
       case _ => total
     }
-    solveRec(0,Seq(),0,0l)
+
+    solveRec(0, Seq(), 0, 0l)
   }
 
 
